98de2fd6f7186eb135571333112f9cd9355fbf9a
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-08-28 16:33:54 -0400

    Extract vote creations into survey model

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,14 +1,19 @@
 get "/surveys" do
+  @surveys = Survey.all
   erb :"surveys/index"
 end
+
 get "/surveys/:id" do
-  @question = Question.new
+  @survey = Survey.find_by(id: params[:id])
   erb :"surveys/show"
 end
+
 post "/surveys/:id" do
-  "add question votes to survey"
-  redirect "/surveys/#{params[:id]}"
+  survey = Survey.find_by(id: params[:id])
+  survey.update_votes(params)
+  redirect "/surveys"
 end
+
 get "/surveys/:id/statistics" do
   erb :"surveys/statistics"
 end
