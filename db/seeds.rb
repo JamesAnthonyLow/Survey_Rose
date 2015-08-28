00559c0ea158a407403e15dfe3378c4c742b51b7
ZedforZardoz <loweja01@gmail.com>
2015-08-28 12:30:48 -0400

    Add seed file using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 require 'faker'
-
-###How To Use Faker
-10.times do
-Post.create(title: Faker::Commerce.product_name, body: Faker::Lorem.paragraph)
+5.times do
+	user_name = Faker::Internet.user_name
+	User.create(username: user_name, password: "foo")
 end
-
+	10.times do
+		survey_name = Faker::App.name
+		survey = Survey.create(name: survey_name)
+		5.times do
+			question = survey.questions.create(body: Faker::Lorem.sentence)
+				4.times do
+					question.options.create(choice: Faker::Lorem.sentence)
+				end
+		end
+		survey.creator = User.all.sample
+		survey.save
+	end
