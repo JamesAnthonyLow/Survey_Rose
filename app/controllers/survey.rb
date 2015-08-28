fe4ad9cff48c8afd9408b52d5d0d4c14497da469
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-08-28 17:25:46 -0400

    add edit routes

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -18,3 +18,16 @@ get "/surveys/:id/statistics" do
   @survey = Survey.find_by(id: params[:id])
   erb :"surveys/statistics"
 end
+
+get "/surveys/:id/edit" do
+  @survey = Survey.find_by(id: params[:id])
+end
+
+put "/surveys/:id" do
+  question = Question.create()
+  options.each do |option|
+
+  end
+  @survey = Survey.find_by(id: params[:id])
+  @survey.questions << question
+end
