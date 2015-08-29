43ed2233418999d58352d9c5ce5a5da4999c655d
ZedforZardoz <loweja01@gmail.com>
2015-08-28 21:18:46 -0400

    refactor questions creation

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
 	belongs_to :survey
 	has_many :options
 	has_many :votes
+	def create_options(choices)
+		choices.values.each do |choice|
+			self.options.create(choice: choice)
+		end
+	end
 end
