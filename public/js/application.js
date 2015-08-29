3fce0fa9a0e512a75f7060f707ac0c0280ffed08
ZedforZardoz <loweja01@gmail.com>
2015-08-29 16:49:04 -0400

    Continue debug of Ajax

--- public/js/application.js
+++ public/js/application.js
@@ -13,14 +13,14 @@ $(document).ready(function () {
         });
 	  });	  
 
-	  $(".newquestionform").submit(function(e) {
+	  $("#addnew").submit(function(e) {
 	  		e.preventDefault();
 	    $.ajax({ 
       		type: $(this).attr("method"),
       		url: $(this).attr("action"),
       		data: $(this).serialize()
         }).done(function(e) {
-          $("#newquestiondiv").append(e);
+          $("#newquestiondiv").append(e.responseText);
         }).fail(function(e) {
           alert(e.statusText);
         });
