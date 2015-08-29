1763dbe3f4549c9e9054b973dedced9af5df8103
Ramon Ramos III & James Lowenthal <pair+arjis02.jamesanthonylow@devbootcamp.com>
2015-08-28 20:55:30 -0400

    Fix update_votes method to assign a voter to the vote

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -4,15 +4,16 @@ class Survey < ActiveRecord::Base
 	has_many :votes
 
 	def already_voted_by_user?(user)
-		votes.map(&:voter).any? {|voter| voter == session[:user]}
+		votes.map(&:voter_id).any? {|voter| voter == user.id}
 	end
 
-  def update_votes(params={})
+  def update_votes(params={}, session_user)
     self.questions.each do |question|
       voter_choice_id = params["#{question.id}"]
       new_vote = question.votes.new
       new_vote.option = Option.find_by(id: voter_choice_id)
       new_vote.survey_id = self.id
+      new_vote.voter = session_user
       new_vote.save
     end
   end
